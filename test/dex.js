40ad60f91777414b943908c793a16480fc6dffc2
Justin Russell <jus.russell@gmail.com>
2013-11-23 22:11:49 -0500

    test: Start building out some tests

---
+++ test/dex.js
@@ -0,0 +1,19 @@
+/*global describe, it, beforeEach */
+
+'use strict';
+
+var expect = require('chai').expect;
+
+describe('Dex', function() {
+  var Dex = require('../lib/dex').Dex
+    , dex;
+
+  //beforeEach(function() {
+  //  dex = new Dex();
+  //});
+
+  describe('making items', function() {
+    var dex = new Dex();
+  });
+  
+});
